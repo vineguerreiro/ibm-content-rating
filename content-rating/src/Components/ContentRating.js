48e6cc061217e33de49167f9196c40e61f76934e
vineguerreiro <vineguerreiro@gmail.com>
2024-10-23 16:01:24 -0300

    Creating my content rating app with React

---
+++ content-rating/src/Components/ContentRating.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './ContentRating.css';
+
+class ContentRating extends Component {
+  constructor() {
+    super();
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      totalRatings: 0,
+      handleLike: () => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      },
+      handleDislike: () => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      }
+    };
+  }
+  render() {
+    return (
+      <>
+        <div className='content-rating'>
+          <p>Click the buttons to like or dislike.</p>
+          <div className='rating-buttons'>
+            <button className="like-button" onClick={this.state.handleLike}>
+              Like ({this.state.likes})
+            </button>
+            <button className="dislike-button" onClick={this.state.handleDislike}>
+              Dislike ({this.state.dislikes})
+            </button>
+          </div>
+          <p>Total Ratings: {this.state.totalRatings}</p>
+        </div>
+      </>
+    );
+  }
+}
+export default ContentRating;
